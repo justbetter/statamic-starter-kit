f09835b056f693664aaf2013ac6893232d3991b4
Kevin Meijer <kevin@justbetter.nl>
2026-04-07 13:47:00 +0200

    Statamic 6 update + new fieldsets

--- export/app/Models/User.php
+++ export/app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +11,7 @@ use Statamic\Notifications\PasswordReset;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     /**
@@ -45,6 +46,7 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'preferences' => 'json',
+            'two_factor_confirmed_at' => 'datetime',
         ];
     }
 
